94a107cc1f4ec0aa0b184c2b7328d552a6ae2ce5
Sheraff <fpellet@ensc.fr>
2025-02-27 19:56:05 +0100

    Inngest: hello world example (w/ fastify + typescript)

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import Fastify from "fastify"
+import { serve } from "inngest/fastify"
+import { inngest, functions } from "./inngest/index.ts"
+
+const fastify = Fastify({
+	logger: true,
+})
+
+fastify.route({
+	method: ["GET", "POST", "PUT"],
+	handler: serve({ client: inngest, functions }),
+	url: "/api/inngest",
+})
+
+fastify.get("/api/hello", async function (request, reply) {
+	await inngest.send({
+		name: "test/hello.world",
+		data: {
+			email: "testUser@example.com",
+		},
+	})
+	return { message: "Event sent!" }
+})
+
+// Start up the fastify server
+fastify.listen({ port: 3000 }, function (err, address) {
+	if (err) {
+		fastify.log.error(err)
+		process.exit(1)
+	}
+})
